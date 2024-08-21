5c52501cb4dc5de8761ef4a053fd604b1ca9de13
Michael Amare <amarem@oregonstate.edu>
2024-08-21 12:31:09 -0500

    game loop

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+from constants import *
+
+# def __init__(self):
+#    pygame.init()
+
+    
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+    print("Starting asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+
+if __name__ == "__main__":
+    main()

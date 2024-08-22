1b6689c197c6847f17c17375d9b0d096ab3e5ee3
Michael Amare <amarem@oregonstate.edu>
2024-08-22 15:21:10 -0500

    2.8 groups

--- main.py
+++ main.py
@@ -7,7 +7,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -15,14 +22,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
-        player.update(dt)
 
 if __name__ == "__main__":
     main()
